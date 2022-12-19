2400a7e173f6215130bf4fe14c3fdd8492a39480
Hyeyeon Seo <42148741+skojphy@users.noreply.github.com>
2022-12-19 11:33:49 +0900

    conf: 프로젝트 초기 세팅
    - sass 설치
    - 단축 경로 설정
    - static/images 폴더 생성
    - 글로벌 스타일 적용

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,22 @@
 import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess({
+		scss: {
+			prependData: `@import "./static/style.scss";`
+		}
+	}),
+
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			'@': path.resolve('./src')
+		}
 	}
 };
 
